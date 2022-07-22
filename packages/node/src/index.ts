d5a631c547731eda0c157b88e4945ee2f5844730
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-07-22 15:10:21 +0300

    Add Yoga V2 migration guide

---
+++ packages/node/src/index.ts
@@ -0,0 +1,5 @@
+console.warn(
+  `@graphql-yoga/node package has been deprecated and will no longer receive updates in the next major.`,
+)
+console.warn(`Please use 'graphql-yoga' npm package instead!'`)
+export * from 'graphql-yoga'
